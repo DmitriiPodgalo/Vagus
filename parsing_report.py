4feb8f829ae2ed5196671ba2792cb0480ae4c725
Yuliya Burankova <15180672+Freddsle@users.noreply.github.com>
2021-11-21 16:09:26 +0300

    added optional out path and img to it

--- parsing_report.py
+++ parsing_report.py
@@ -1,9 +1,11 @@
 import logging
 import datetime
-from types import new_class
-import os
+import os, shutil
+import re
 import pandas as pd
 
+from types import new_class
+from pathlib import Path
 import typer
 import jinja2
 
@@ -25,15 +27,25 @@ def raise_error(text):
     raise Exception(text)
 
 
+def prepare_outdir(outdir):
+    '''
+    Check if outdir exists, if not - create it.
+    Also check if images for status chesk exists, if not - create it.
+    '''
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    if not os.path.exists(outdir + 'check_img/error.jpg'):
+        shutil.copytree('./Report_data/check_img/', outdir + 'check_img/', dirs_exist_ok=True)
+
+
 def prepair_data(input, outdir):
     '''
     Parsed file and create quality checks and plots.
     Save plots to "outdit/Report_data/".
     '''
     parsed_file = fastqc.read_file(input)
-    
-    if not os.path.exists(outdir):
-            os.makedirs(outdir)
+    prepare_outdir(outdir)
     
     # 1-3
     sequence_length_distribution_result = fastqc.sequence_length_distribution(parsed_file, outdir)
@@ -44,13 +56,13 @@ def prepair_data(input, outdir):
 
     qualities_per_base = FastQC_functions.calculate_quality_per_base(parsed_file_for_func)
     dict_mean_qual = FastQC_functions.calculate_mean_quality_per_base(qualities_per_base)
-    FastQC_functions.plot_per_base_seq_quality(qualities_per_base, dict_mean_qual)
+    FastQC_functions.plot_per_base_seq_quality(qualities_per_base, dict_mean_qual, outdir)
     # 5
     d = FastQC_functions.per_sequence_quality(parsed_file_for_func)
-    FastQC_functions.plot_per_seq_quality_scores(d)
+    FastQC_functions.plot_per_seq_quality_scores(d, outdir)
     # 6
     lst_proportions = FastQC_functions.per_base_nucleotides_proportion(parsed_file_for_func)
-    FastQC_functions.plot_per_base_seq_content(lst_proportions)
+    FastQC_functions.plot_per_base_seq_content(lst_proportions, outdir)
     # 7
     gc_content_result = FastQC_G.draw_gc_content(parsed_file, outdir)
     # 8
@@ -120,15 +132,44 @@ def render_report(context, template, outdir):
     logging.info('report created')
 
 
+def check_outdir(outdir):
+    '''
+    Check the last simbol in outdir.
+    If in not the '/' - added it to path.
+    '''
+    pattern_end = re.compile(r".*/$")
+    pattern_start = re.compile(r"\./.*/$")
+
+    if not pattern_end.match(outdir):
+        outdir = outdir + '/'
+    
+    if not pattern_start.match(outdir):
+        outdir = './' + outdir
+    
+    return outdir
+
+
 DEFAULT_TEMPLATE = './Report_templates/report.html.j2'
 DEFAULT_OUTPUT_DIR = './Report_data/'
 
 
 @app.command()
-def generate(input: str = typer.Option(...),
+def generate(input: Path = typer.Option(...,
+                                        "--input", "-i",
+                                        help="Path to input file from Vagus repository",
+                                        exists=True,
+                                        file_okay=True,
+                                        dir_okay=False,
+                                        writable=False,
+                                        readable=True,
+                                        resolve_path=True),
+             outdir: str = typer.Option(DEFAULT_OUTPUT_DIR,
+                                        "--outdir", "-o",
+                                        help="Path to analysis output directory from Vagus repository"),
              template: str = DEFAULT_TEMPLATE,
-             outdir: str = DEFAULT_OUTPUT_DIR,
              log_level: str = 'info'):
+
+    outdir = check_outdir(outdir)
 
     context = prepair_data(input, outdir)
     render_report(context, template, outdir)
